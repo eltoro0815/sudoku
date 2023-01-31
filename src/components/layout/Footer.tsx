e9f323a86980022b440c4c9e9e242d5009124c50
Thorsten Ruppenstein <th_ru@web.de>
2023-01-31 23:02:29 +0100

    added link to Github repository in the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,6 +7,7 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <p>&#169; 2020 Amith Raravi - source code on <a href="https://github.com/raravi/sudoku">Github</a></p>
+      <p>2022 forked by eltoro0815 - source code on <a href="https://github.com/eltoro0815/sudoku">Github</a></p>
     </footer>
   )
 }
